b8df340ce1d367063291050cc766753ccbb9aaee
dhia-zeddini <80212396+dhia-zeddini@users.noreply.github.com>
2023-11-15 02:57:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ import  express from 'express';
 const app =express()
 const port=process.env.PORT;
 
-
+app.get("/", (req, res) => {
+  res.send("hello");
+});
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
   });
