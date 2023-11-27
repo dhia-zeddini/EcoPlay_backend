fddf181f904755f8b125e66ad0252d998e99b816
rekikmarwa <113935930+rekikmarwa@users.noreply.github.com>
2023-11-27 13:39:12 +0100

    pre mrege

--- models/QuizRespo.js
+++ models/QuizRespo.js
@@ -1,11 +1,9 @@
 import mongoose from 'mongoose';
 import db from '../config/DBConnection.js';
 const { Schema } = mongoose;
-
 const QuizResultSchema = new Schema({
   category: { type: String, required: true },
   type: { type: String, required: true },
-  difficulty: { type: String, required: true },
   question: { type: String, required: true },
   correct_answer: { type: String, required: true },
   incorrect_answers: { type: [String], required: true },
